d29998c7b2fdd71c1fec65a3175456c132463986
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2026-08-24 01:07:41 +0300

    ل

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -545,7 +545,7 @@
                 <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
               </div>
               <div class="text-slate-900 dark:text-white font-bold mb-1">{{ i === 1 ? $t('contact.phone') : $t('contact.email') }}</div>
-              <div class="text-slate-600 dark:text-slate-400 text-sm">{{ i === 1 ? '+31 6 84094271' : 'info@moveitmaastricht.nl' }}</div>
+              <div class="text-slate-600 dark:text-slate-400 text-sm">{{ i === 1 ? '+31 6 34837587' : 'info@moveitmaastricht.nl' }}</div>
             </div>
           </div>
 
