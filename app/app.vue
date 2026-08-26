d94d661e3c50185c7b5ed49149ab43fcc94a9a14
Mostafa Zayed <143009621+MostafaZayed72@users.noreply.github.com>
2026-08-26 12:19:34 +0300

    d

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe 
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NR8924FH"
+        height="0" 
+        width="0" 
+        style="display:none;visibility:hidden"
+      ></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
